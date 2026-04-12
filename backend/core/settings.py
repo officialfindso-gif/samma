901495844e9bb803b22703301171750740aa8a54
Estate Invest <official.findso@gmail.com>
2026-04-12 18:00:26 +0300

    fix: add STATIC_ROOT and MEDIA_ROOT for production deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -244,6 +244,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = []
+
+# Media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
